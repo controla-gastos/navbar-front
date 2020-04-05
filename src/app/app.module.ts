d49a006bc584e7e0a133b99ec01fb3184183c40d
Artur Ribeiro <artur.adr@hotmail.com>
2020-04-05 16:40:52 -0300

    finalizado navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent
   ],
   imports: [
     BrowserModule,
